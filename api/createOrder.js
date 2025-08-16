ff49b85e3db6ecc408405472f73dc6d871952d3e
anmol-narain <anmolnarain83@gmail.com>
2025-08-16 20:03:52 +0530

    debugged

--- api/createOrder.js
+++ api/createOrder.js
@@ -2,6 +2,8 @@
 
 import Razorpay from "razorpay";
 
+console.log("RAZORPAY_KEY_ID:", process.env.RAZORPAY_KEY_ID ? "Loaded ✅" : "Missing ❌");
+
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
